d53ac569ac8d7b60e13ec7393357fa78b5d684ff
jay soni <jaysoni43799@gmail.com>
2026-04-23 20:18:46 +0530

    update till 34:11

--- tools.py
+++ tools.py
@@ -7,19 +7,28 @@ from dotenv import load_dotenv
 from rich import print
 load_dotenv()
 
-tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY")) 
 
 @tool
-def web_search(query : str) -> str:
-    """Search the web for recent and reliable information  ona toipc.Returns titles, URLs and snippets """
-    results = tavily.search(query=query, max_results=5)
+def comprehensive_search(query : str) -> str:
+    """Search the web and YouTube for recent and reliable information on a topic. Returns titles, URLs and snippets and youtube video links if available"""
+    
+    # 1. Fetch general web articles
+    web_results = tavily.search(query=query, max_results=5)
+    
+    # 2. Fetch specific YouTube videos
+    yt_results = tavily.search(query=query, search_depth="advanced", include_domains=["youtube.com"], max_results=3)
 
     out = []
+    
+    for r in web_results.get('results', []):
+        out.append(f"Title: {r['title']} \nURL: {r['url']} \nSnippet: {r['content'][:300]} \n")
 
-    for r in results['results']:
-        out.append(f"Title:{r['title']} \nURL:{r['url']} \nSnippet:{r['content'][:300]}")
+    
+    for r in yt_results.get('results', []):
+        out.append(f"Video Title: {r['title']} \nYouTube Link: {r['url']} \nDescription: {r['content'][:500]} \n")
 
     return "\n----\n".join(out)
 
-print(web_search.invoke( "What is the update on war"))
+print(comprehensive_search.invoke("What is the latest news about gold?"))
 
